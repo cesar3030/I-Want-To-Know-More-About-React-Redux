be6722bd46d5fbb86f8ebaa138af5ce1da415afe
César Jeanroy <cesar.jeanroy@gmail.com>
2018-08-26 22:07:52 -0400

    Fixing silly mistakes

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,17 +1,2 @@
 import { ADD_ARTICLE } from "../constants/action-types";
-
-const initialState = {
-  articles: []
-};
-
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_ARTICLE:
-      state.articles.push(action.payload);
-      return state;
-    default:
-      return state;
-  }
-};
-
-export default rootReducer;
+export const addArticle = article => ({ type: ADD_ARTICLE, payload: article });
